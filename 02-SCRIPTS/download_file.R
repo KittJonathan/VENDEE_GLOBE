9684d1914ac61ca5d0a4f6948d538fd36f00d070
Jonathan Kitt <jonathan.kitt@inrae.fr>
2024-12-02 11:30:34 +0100

    get latest standings

--- 02-SCRIPTS/download_file.R
+++ 02-SCRIPTS/download_file.R
@@ -1,7 +1,26 @@
 # Download files
 
-date <- "20241111"
-time <- "020000"
+# df <- tibble(date = c(rep("20241129", 4), rep("20241130", 6), rep("20241201", 6), rep("20241202", 2)),
+#        time = c("100000", "140000", "180000", "220000",
+#                 "020000", "060000", "100000", "140000", "180000", "220000",
+#                 "020000", "060000", "100000", "140000", "180000", "220000",
+#                 "020000", "060000"))
+
+# df <- df |> 
+#   mutate(url = paste0("https://www.vendeeglobe.org/sites/default/files/ranking/vendeeglobe_leaderboard_",
+#                       date, "_", time, ".xlsx"),
+#          path = paste0("01-DATA_RAW/vendeeglobe_leaderboard_", date, "_", time, ".xlsx"))
+
+# for (i in 1:nrow(df)) {
+#   
+#   download.file(url = df$url[i],
+#                 destfile = df$path[i],
+#                 mode = "wb")
+#   
+# }
+
+date <- "20241202"
+time <- "100000"
 
 url <- paste0("https://www.vendeeglobe.org/sites/default/files/ranking/vendeeglobe_leaderboard_",
               date, "_", time, ".xlsx")
