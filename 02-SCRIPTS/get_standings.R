c7395617726f4a647dddf0f0ccb5656874402a0a
Jonathan Kitt <jonathan.kitt@inrae.fr>
2025-01-14 15:49:40 +0100

    update standings

--- 02-SCRIPTS/get_standings.R
+++ 02-SCRIPTS/get_standings.R
@@ -50,12 +50,12 @@ get_standings <- function(date = NULL, time = NULL, from = NULL, to = NULL, dir)
   }
 }
 
-get_standings(date = "20250113", time = "020000", dir = "01-DATA_RAW/")
-get_standings(date = "20250113", time = "060000", dir = "01-DATA_RAW/")
-# get_standings(date = "20250113", time = "100000", dir = "01-DATA_RAW/")
-# get_standings(date = "20250113", time = "140000", dir = "01-DATA_RAW/")
-# get_standings(date = "20250113", time = "180000", dir = "01-DATA_RAW/")
-# get_standings(date = "20250113", time = "220000", dir = "01-DATA_RAW/")
+get_standings(date = "20250114", time = "020000", dir = "01-DATA_RAW/")
+get_standings(date = "20250114", time = "060000", dir = "01-DATA_RAW/")
+get_standings(date = "20250114", time = "100000", dir = "01-DATA_RAW/")
+get_standings(date = "20250114", time = "140000", dir = "01-DATA_RAW/")
+# get_standings(date = "20250114", time = "180000", dir = "01-DATA_RAW/")
+# get_standings(date = "20250114", time = "220000", dir = "01-DATA_RAW/")
 
 get_standings(from = "20250101", to = "20250105", dir = "01-DATA_RAW/")
 
