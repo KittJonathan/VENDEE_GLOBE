da04f1f20c466b7b4a4fc828a4c24854bbe447b9
Jonathan Kitt <jonathan.kitt@inrae.fr>
2025-01-17 17:02:14 +0100

    update standings -> 2025-01-17 @ 14:00:00

--- 02-SCRIPTS/get_standings.R
+++ 02-SCRIPTS/get_standings.R
@@ -52,8 +52,8 @@ get_standings <- function(date = NULL, time = NULL, from = NULL, to = NULL, dir)
 
 get_standings(date = "20250117", time = "020000", dir = "01-DATA_RAW/")
 get_standings(date = "20250117", time = "060000", dir = "01-DATA_RAW/")
-# get_standings(date = "20250117", time = "100000", dir = "01-DATA_RAW/")
-# get_standings(date = "20250117", time = "140000", dir = "01-DATA_RAW/")
+get_standings(date = "20250117", time = "100000", dir = "01-DATA_RAW/")
+get_standings(date = "20250117", time = "140000", dir = "01-DATA_RAW/")
 # get_standings(date = "20250117", time = "180000", dir = "01-DATA_RAW/")
 # get_standings(date = "20250117", time = "220000", dir = "01-DATA_RAW/")
 
